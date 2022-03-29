a7a85d37215c86fce74f5ceeed6e41eebdd3f62a
Frans Lytzen <flytzen@neworbit.co.uk>
2022-03-29 17:39:27 +0100

    Removing linting errors so I can do a test deploy

--- src/Notification.js
+++ src/Notification.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
+
 
 export default function Notification() {
   // const [status, setStatus] = useState(Notification.permission);
