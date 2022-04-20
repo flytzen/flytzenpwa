175dd7a7419de57833f86d69e301c558446344c6
Frans Lytzen <flytzen@neworbit.co.uk>
2022-04-20 16:00:18 +0000

    Changed to use minimal APIs feature

--- func/Message.cs
+++ func/Message.cs
@@ -1,31 +1,24 @@
-using System.Collections.Generic;
-using System.Net;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
+namespace func;
 
-namespace func
+public class Message
 {
-    public class Message
+    private readonly ILogger _logger;
+
+    public Message(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
+        _logger = loggerFactory.CreateLogger<Message>();
+    }
 
-        public Message(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<Message>();
-        }
+    [Function("Message")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        [Function("Message")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString("Welcome to Azure Functions!");
 
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
-        }
+        return response;
     }
 }
---
+++ func/globalusings.cs
@@ -0,0 +1,4 @@
+global using Microsoft.Azure.Functions.Worker;
+global using Microsoft.Azure.Functions.Worker.Http;
+global using Microsoft.Extensions.Logging;
+global using System.Net;
