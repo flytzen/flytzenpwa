4c3f3bc8851ad2ae868d03cc2e0ccf79affb97a4
Frans Lytzen <flytzen@neworbit.co.uk>
2022-06-20 11:05:00 +0100

    Misc

--- app/src/App.js
+++ app/src/App.js
@@ -8,29 +8,34 @@ import {
 import Api from "./Api"
 import BrowserNotifications from './BrowserNotifications';
 import Home from './Home';
-
+import CssBaseline from '@mui/material/CssBaseline';
+import TopNavBar from './TopNavBar';
 
 function App() {
   return (
+    <React.Fragment>
+      <CssBaseline />
+      <TopNavBar />
+      <div>
+        <h1>Hello</h1>
 
-    <div>
-      <h1>Hello</h1>
-      <BrowserRouter>
+        <BrowserRouter>
+// https://mui.com/material-ui/guides/routing/#link
         <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/funccall">API</Link></li>
-            <li><Link to="/browsernotifications">Notifications</Link></li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/funccall" element={<Api />} />
-          <Route path="/browsernotifications" element={<BrowserNotifications />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/funccall">API</Link></li>
+              <li><Link to="/browsernotifications">Notifications</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/funccall" element={<Api />} />
+            <Route path="/browsernotifications" element={<BrowserNotifications />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </React.Fragment>
   );
 }
 
